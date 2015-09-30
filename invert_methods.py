89d0725538b1144c7e5e1ed347d3f97412bee553
radar <radar@usf-wifi-224-26-99.laptops.usf.edu>
2015-09-30 10:30:14 -0400

    INITIAL COMMIT

---
+++ invert_methods.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 20:49:14 2015
+
+@author: radar
+
+contains the general inverse solutionss
+"""
+
---
+++ matrix_ops.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 20:47:00 2015
+
+@author: radar
+
+contains the general functions for handeling matricies 
+"""
+
