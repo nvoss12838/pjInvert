19a6892d78d1180e37c9db62e2f404ce00863ce1
radar <radar@usf-wifi-224-24-27.laptops.usf.edu>
2015-10-06 19:55:30 -0400

    create python classes for holding the matrixes for inversions

---
+++ data_vector.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+class for holding the data_vector for inversion problems
+
+USF Geodesy Lab Fall 2015
+
+Nick Voss
+"""
+import pandas as pg
+import numpy as np 
+
+class data(object):
+    '''
+    holds the data vector and contains methods for manipulation
+    
+    Attributes:
+        dataS = pandas series with observations index should be th observation names
+        weight = pandas series with weights for observations, index is observation names
+    Methods:
+        
+    '''
+    def __init__(self,dataS,weightS):
+        self.dataS = dataS
+        self.weights = weightS
+    
+    def weight2matirx(self):
+        return np.diag(self.weights)
+        
+    
+
--- invert_methods.py
+++ invert_methods.py
@@ -1,9 +1,41 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 29 20:49:14 2015
+solutions to the inverse problem
+relies on other classes in pjInvert
 
-@author: radar
+USF Geodesy Fall 2015
 
-contains the general inverse solutionss
+Nick Voss
 """
+from numpy.linalg import inv
+import pandas as pd 
 
+def weightedLeastSquares(greensfnc,Wm,We,epsilon,m_init,data):
+    '''
+    performs a weighted Least Squares inversion
+    m^{est}=[G^TW_mG + \varepsilon^2W_m]^{-1}[G^{T}W_ed +\varepsilon^2W_m\langle m \rangle]$
+    
+    inputs:
+        greensfnc = greens function object
+        Wm = regularization matrix 
+        epsilon = factor contoling regularization enforcement
+        m_init = initial guess, a model oject
+        data = data object
+    returns:
+        m_est = estmates model paramaters 
+    '''
+    #make first hald of equation
+    G = greensfnc.as_matrix()
+    GT = greensfnc.transpose().as_matrix()
+    first = GT*Wm*G + epsilon.squared()*Wm
+    firstInv = inv(first)
+    #make second half
+    second = GT*We*data.values + epsilon.squared()*Wm*m_init.values
+    solution = firstInv*second
+    #make m object from solution
+    m_est = pd.Series(solution,index = m_init.index)
+    return m_est
+    
+    
+    
